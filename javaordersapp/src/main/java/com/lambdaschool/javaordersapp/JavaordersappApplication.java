8849582d36939084fbc991c4a970a14d410202cb
Christopher Corvo <christophercorvo@gmail.com>
2020-12-09 19:45:18 -0500

    Initial Commit

---
+++ javaordersapp/src/main/java/com/lambdaschool/javaordersapp/JavaordersappApplication.java
@@ -0,0 +1,16 @@
+package com.lambdaschool.javaordersapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaordersappApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(JavaordersappApplication.class,
+            args);
+    }
+
+}
